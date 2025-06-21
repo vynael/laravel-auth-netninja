f44bf1813f031f5a61b6146b8a585770b38a7c37
vynael <vynettekaito@gmail.com>
2025-06-21 21:50:29 +0700

    final step from vyn mac, done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,27 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ninja Network</title>
-  
-  @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network</title>
+
+    @vite('resources/css/app.css')
 
 </head>
-<body class="text-center px-8 py-12">
-  <h1>Welcome to the Ninja Network</h1>
-  <p>Click the button below to view the list of ninjas.</p>
 
-  <a href="/ninjas" class="btn mt-4 inline-block">
-    Find Ninjas!
-  </a>
+
+<body class="text-center">
+    @if (session('logged_in'))
+        <div id="flash" class="p-4 text-center bg-yellow-50 text-yellow-500 font-bold mb-3">
+            {{ session('logged_in') }}
+        </div>
+    @endif
+    <h1>Welcome to the Ninja Network</h1>
+    <p>Click the button below to view the list of ninjas.</p>
+
+    <a href="/ninjas" class="btn mt-4 inline-block">
+        Find Ninjas!
+    </a>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,38 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NinjaController;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
+    Session::flash('logged_in', 'You already logged in');
     return view('welcome');
 });
 
 
 // Authentication routes
-// register 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-// login 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-// post 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-// login 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+// guest middleware is used to protect routes that should not be accessible to authenticated users
 // logout
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    // register 
+    Route::get('/register', 'showRegister')->name('show.register');
+    // login 
+    Route::get('/login', 'showLogin')->name('show.login');
+    // post 
+    Route::post('/register', 'register')->name('register');
+    // login 
+    Route::post('/login', 'login')->name('login');
+});
+// auth user middleware is used to protect routes that should only be accessible to authenticated users
+Route::middleware('auth')->controller(NinjaController::class)->group(function () {
+    // Dojo routes
+    Route::get('/ninjas', 'index')->name('ninjas.index');
+    Route::get('/ninjas/create', 'create')->name('ninjas.create')->middleware('auth');
+    Route::get('/ninjas/{ninja}', 'show')->name('ninjas.show');
+    Route::post('/ninjas', 'store')->name('ninjas.store');
+    Route::delete('/ninjas/{ninja}', 'destroy')->name('ninjas.destroy');
+});
 
 
-
-// Dojo routes
-Route::get('/ninjas', [NinjaController::class, 'index'])->name('ninjas.index');
-Route::get('/ninjas/create', [NinjaController::class, 'create'])->name('ninjas.create');
-Route::get('/ninjas/{ninja}', [NinjaController::class, 'show'])->name('ninjas.show');
-Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
-Route::delete('/ninjas/{ninja}', [NinjaController::class, 'destroy'])->name('ninjas.destroy');
+// notes
+// by default, Laravel use 2 middleware = auth, and guest
